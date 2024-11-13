8e17b084f9b67f40055f05d3091ea5d9a0fafd16
Mehmet Celik <mehmet@celik.be>
2024-11-13 22:59:03 +0100

    updates of model and constructs

--- lib/elastic/query/parser.rb
+++ lib/elastic/query/parser.rb
@@ -301,7 +301,7 @@ module Query
           next_term = @parsed[next_term_index]
           #
           next if !prev_term[:type].eql?('term')
-          if ['*', '?', '-', ',', '!', '@', '#', '$', ';', ':', '/', '%', '^', '&', '"', '|', '+', '\'', "~", '`', '<', '>', '(', ')'].include?(term[:value])
+          if ['?', '-', ',', '!', '@', '#', '$', ';', ':', '/', '%', '^', '&', '"', '|', '+', '\'', "~", '`', '<', '>', '(', ')'].include?(term[:value])
             # if !['(',')'].include?(term[:value])
             prev_term[:value] += term[:value]
             term[:delete] = true
